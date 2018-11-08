c92a71d736177e7d7abc4e3c4035107c77ac35a5
ThatJazzyGuy <minerscreedutube@gmail.com>
2018-11-08 16:34:06 -0600

    Initial Commit with Preprocessing

---
+++ preprocess.py
@@ -0,0 +1,32 @@
+import csv
+
+files = ['October2018.csv', 'September2018.csv']
+data = []
+linesread = 0
+for file in files:
+    with open(file) as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            linesread += 1
+            checkFlag = False
+            temp = row
+            #column6
+            if data == []:
+                data.append(temp)
+            else:
+                for rows in data:
+                    #print(temp[8] + ' ' + rows[8])
+                    if temp[8] == rows[8]:
+                        checkFlag = True
+                    if checkFlag:
+                        break
+                if checkFlag:
+                    pass
+                else:
+                    data.append(temp)
+with open('aggregated_data.csv',mode= 'w') as outfile:
+    writer = csv.writer(outfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+    for row in data:
+        writer.writerow(row)
+print(linesread)
+print(data)
