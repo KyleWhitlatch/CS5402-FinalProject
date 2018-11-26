502bb5487a52784aab42d654b908cc6b2e575b38
jschultz1995 <43323187+jschultz1995@users.noreply.github.com>
2018-11-26 16:17:40 -0600

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
 import csv
+from sklearn import tree
+import pandas as pd
 
 files = ['September2018.csv', 'October2018.csv']
 data = []
@@ -15,8 +17,18 @@ for file in files:
                 data.append(temp)
             else:
                 for rows in data:
+                    #3,4,5,6,7,14,15
+                    del temp[3]
+                    del temp[4]
+                    del temp[5]
+                    del temp[6]
+                    del temp[7]
+                    del temp[14]
+                    del temp[15]
                     #print(temp[8] + ' ' + rows[8])
                     if '2018' not in temp[2]:
+                        checkFlag = True
+                    elif (temp[18] == "0" and temp[19] == "0")
                         checkFlag = True
                     if checkFlag:
                         break
@@ -30,3 +42,7 @@ with open('aggregated_data.csv',mode= 'w') as outfile:
         writer.writerow(row)
 #print(linesread)
 #print(data)
+
+#need to split the data into the target(prediction) feature and the rest
+#data = pd.read_csv('aggregated_data.csv')
+#clf = tree.DecisionTreeClassifier()
