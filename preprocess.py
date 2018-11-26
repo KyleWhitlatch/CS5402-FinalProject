e55a0a5e3bbba37e8b59518074c6f2fec320296f
jschultz1995 <43323187+jschultz1995@users.noreply.github.com>
2018-11-26 17:05:45 -0600

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,12 @@
 import csv
 from sklearn import tree
 import pandas as pd
+import numpy as np
 
 files = ['September2018.csv', 'October2018.csv']
 data = []
 linesread = 0
+colsToDelete = [3,4,5,6,7,11,12,13,14,15,16,17]
 for file in files:
     with open(file) as f:
         reader = csv.reader(f, delimiter=',')
@@ -14,27 +16,23 @@ for file in files:
             temp = row
             #column6
             if data == []:
+                temp = np.delete(temp,colsToDelete).tolist()
                 data.append(temp)
             else:
                 for rows in data:
-                    #3,4,5,6,7,14,15
-                    del temp[3]
-                    del temp[4]
-                    del temp[5]
-                    del temp[6]
-                    del temp[7]
-                    del temp[14]
-                    del temp[15]
+                    #print (len(temp))
                     #print(temp[8] + ' ' + rows[8])
                     if '2018' not in temp[2]:
                         checkFlag = True
-                    elif (temp[18] == "0" and temp[19] == "0")
+                    elif (temp[18] == "0" and temp[19] == "0"):
                         checkFlag = True
                     if checkFlag:
                         break
                 if checkFlag:
                     pass
                 else:
+                    temp = np.delete(temp,colsToDelete).tolist()
+                    #print(temp)
                     data.append(temp)
 with open('aggregated_data.csv',mode= 'w') as outfile:
     writer = csv.writer(outfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
@@ -43,6 +41,8 @@ with open('aggregated_data.csv',mode= 'w') as outfile:
 #print(linesread)
 #print(data)
 
+#The Lines below can be put into a different python script so that we aren't preprocessing data everytime
 #need to split the data into the target(prediction) feature and the rest
 #data = pd.read_csv('aggregated_data.csv')
+#print (data.head())
 #clf = tree.DecisionTreeClassifier()
